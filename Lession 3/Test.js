a901b2ada780913c3288d18ff14c8cea08a0302c
Nguyen Phuong Nam <namnp.201002@gmail.com>
2021-10-30 02:37:49 +0700

    update

---
+++ Lession 3/Test.js
@@ -0,0 +1,9 @@
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 2, "Lemon", "Kiwi");
+// document.getElementById("title").innerHTML = fruits.toString();
+console.log(fruits);
+function _TEST() {
+    alert('Hi chao moi nguoi');
+}
+_TEST();
